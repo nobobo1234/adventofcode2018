d5d6505b7a2fc96262087adb63871ca0561ec0c2
nobobo1234 <noahvanboven@gmail.com>
2018-12-16 21:45:20 +0000

    Horrible solution for part 2 help me

--- day1/day1.js
+++ day1/day1.js
@@ -12,4 +12,21 @@ const part1 = () => {
     return sum;
 }
 
-console.log(part1());
+const part2 = () => {
+    const frequencies = fs.readFileSync(`${__dirname}/day1input.txt`).toString().split('\n');
+    const sums = [];
+    let i = 0,
+        sum = 0;
+    while(i !== frequencies.length + 1) {
+        if(i === frequencies.length) i = 0;
+
+        sum += Number(frequencies[i]);
+        if(sums.includes(sum)) return sum;
+
+        sums.push(sum);
+        i++;
+    }
+}
+
+console.log(`Solution to day1 part 1 is: ${part1()}`);
+console.log(`Solution to day1 part 2 is: ${part2()}`);
