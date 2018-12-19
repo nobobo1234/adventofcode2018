ff7ab9210ba7d66b3e8253c8eb5aefe494ff6a32
nola1222 <noahvanboven@gmail.com>
2018-12-19 19:26:50 +0100

    Adding day 3 part 1

---
+++ day3/day3.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const flatten = array => [].concat(...array);
+
+const part1 = () => {
+    const claims = fs.readFileSync(`${__dirname}/day3input.txt`).toString().split('\n')
+        .map(el => ({
+            coordinates: el.split(' @ ')[1].split(': ')[0].split(',').map(Number),
+            dimensions: el.split(' @ ')[1].split(': ')[1].split('x').map(Number)
+        }));
+    const array = Array.from({ length: 1000 }, a => Array.from({ length: 1000 }))
+    for(const claim of claims) {
+        const coordinates = claim.coordinates,
+            dimensions = claim.dimensions;
+        for(let y = coordinates[1]; y < coordinates[1] + dimensions[1]; y++) {
+            for(let x = coordinates[0]; x < coordinates[0] + dimensions[0]; x++) {
+                if(array[y][x] === true) {
+                    array[y][x] = 1; // Value is overlapped
+                    continue;
+                } else if(array[y][x] === 1) {
+                    continue;
+                } else {
+                    array[y][x] = true; // Value is taken once
+                }
+            }
+        }
+    }
+    return flatten(array).filter(el => el === 1).length;
+}
+
+console.log(`The solution to day 3 part 1 is: ${part1()}`);
