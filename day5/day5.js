6cb25ec365f996a9d66af0b5f3ce6a2e1b1b829c
nola1222 <noahvanboven@gmail.com>
2018-12-22 12:59:22 +0100

    Day 5 part 2 in progress

--- day5/day5.js
+++ day5/day5.js
@@ -18,4 +18,27 @@ const part1 = () => {
     return polymer.length;
 }
 
+const part2 = () => {
+    const polymer = fs.readFileSync(`${__dirname}/day5input.txt`).toString().split('');
+    let shortestLength = polymer.length;
+    for(let i = 97; i < 97 + 26; i++) {
+        const letter = String.fromCharCode(i);
+        const filteredPolymer = polymer.filter(unit => !sameChar(letter, unit));
+        for(let i = filteredPolymer.length - 1; i > 0; i--) {
+            const currU = i !== filteredPolymer.length ? filteredPolymer[i] : filteredPolymer[i-1];
+            const prevU = i !== filteredPolymer.length ? filteredPolymer[i-1] : filteredPolymer[i-2];
+            if(isUpperCase(currU) && isLowerCase(prevU) && sameChar(currU, prevU)) {
+                filteredPolymer.splice(i-1, 2);
+            } else if(isLowerCase(currU) && isUpperCase(prevU) && sameChar(currU, prevU)) {
+                filteredPolymer.splice(i-1, 2);
+            }
+        }
+        if(filteredPolymer.length < shortestLength) {
+            shortestLength = filteredPolymer.length;
+        }
+    }
+    return shortestLength;
+}
+
 console.log(`The solution to day 5 part 1 is ${part1()}`);
+console.log(`The solution to day 5 part 2 is ${part2()}`);
