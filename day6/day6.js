9ff46f2faef392c92b3a60c04ba23912e70e57d5
nola1222 <noahvanboven@gmail.com>
2018-12-23 15:24:10 +0100

    Finished day 6 part 1

--- day6/day6.js
+++ day6/day6.js
@@ -11,17 +11,31 @@ const part1 = () => {
         if(coordinate[1] < minY) minY = coordinate[1];
     }
     console.log(`Min: (${minX}, ${minY}), Max: (${maxX}, ${maxY})`)
-    const array = Array.from({ length: maxY + 1 }, a => Array.from({ length: maxX + 1 }));
-    for(let y = minY; y < array.length; y++) {
-        for(let x = minX; y < array[y].length; x++) {
+    const areas = Array.from({ length: coordinates.length }, e => 0);
+    for(let y = minY; y <= maxY; y++) {
+        for(let x = minX; x <= maxX; x++) {
             let lowestDistance = Math.abs(x - coordinates[0][0])+Math.abs(y-coordinates[0][1]);
-            for(const point of coordinates) {
+            let lowestCoordinate = 0;
+            coordinates:
+            for(let i = 1; i < coordinates.length; i++) {
+                const point = coordinates[i];
                 const dist = Math.abs(x - point[0])+Math.abs(y-point[1]);
-                if(dist < lowestDistance) lowestDistance = dist;
+                if(dist < lowestDistance) {
+                    lowestDistance = dist
+                    lowestCoordinate = i;
+                } else if(dist === lowestDistance) {
+                    lowestCoordinate = null;
+                    continue coordinates;
+                }
             }
-            array[y][x] = lowestDistance;
+            if(y === 0 || x === 0 || y === maxY || x === maxX) {
+                areas[lowestCoordinate] = null;
+            } else if(lowestDistance !== null) {
+                areas[lowestCoordinate] += 1;
+            }
         }
     }
+    return Math.max(...areas.filter((e, i) => e !== null));
 }
 
 console.log(part1());
